01f6640ffc5e706f0239efc84bc059eba566451f
sivabalan-sv01 <svsiva934@gmail.com>
2025-07-21 00:40:35 +0530

    Two sum leet code problem

---
+++ DSA_Problems/Two_sum.py
@@ -0,0 +1,16 @@
+#brute_force approach
+def twosum(nums,target): #nums is the list of numbers
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i]+nums[j]==target:
+                return [i,j]
+
+# hash map approach
+
+def twoSum (nums, target ):
+    num_map={}
+    for i,num in enumerate(nums):
+        comp= target - num
+        if comp in num_map:
+            return [num_map(comp),i]
+        num_map(comp)=i
